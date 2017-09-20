d858583b4f3d985b7dafd6b9c2d452589663a99a
edmond120 <edmondwong888@msn.com>
2017-09-20 14:49:56 -0400

    fixed bug

--- basicFlaskApp.py
+++ basicFlaskApp.py
@@ -7,9 +7,9 @@ def root():
     return '/route1\n/route2'
 @my_app.route('/1')
 def route1():
-	return 'route1'
+    return 'route1'
 @my_app.route('/2')
-	return 'route2'
+    return 'route2'
 
 if __name__ == '__main__':
     my_app.run()
